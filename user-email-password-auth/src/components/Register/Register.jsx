842e8876631c02fb0820447870a082b278a41548
Shahadoth-ali <md.sahadat.live@gmail.com>
2024-02-24 04:00:02 +0600

    17th commit

--- user-email-password-auth/src/components/Register/Register.jsx
+++ user-email-password-auth/src/components/Register/Register.jsx
@@ -1,8 +1,9 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, sendEmailVerification, updateProfile } from "firebase/auth";
 import auth from "../../firebase/firebase.config";
 import { useState } from "react";
 import { FaRegEyeSlash } from "react-icons/fa";
 import { IoEyeOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 
 const Register = () => {
@@ -15,10 +16,11 @@ const Register = () => {
     const handleSubmit=e=>{
         e.preventDefault();
         console.log('form submitted.');
+        const name=e.target.name.value;
         const email=e.target.email.value;
         const password=e.target.password.value;
-        console.log(email);
-        console.log(password);
+        const accepted=e.target.terms.checked;
+      console.log(name,email,password);
 
           //setting error null
           setRegisterError('');
@@ -30,9 +32,13 @@ const Register = () => {
             setRegisterError('Password should be at least 6 characters or longer');
             return;
         }
-        else if(!/^[a-zA-Z0-9!@#$%^&*]/.test(password)){
+        else if(!/[A-Z]/.test(password)){
             setRegisterError("password should contain atleast one number and one special character");
             return;
+        }
+        else if(!accepted){
+          setRegisterError('Please accept our terms and conditions');
+          return;
         }
       
         //creating user for firebase
@@ -40,6 +46,20 @@ const Register = () => {
         .then(result=>{
             console.log(result.user);
             setSuccess('User created successfully.');
+
+            //update profile
+            updateProfile(result.user,{
+              displayName:name,
+              photoURL:null
+            })
+            .then(()=>console.log('profile updated'))
+            .catch()
+
+            //send verification email
+            sendEmailVerification(result.user)
+            .then(()=>{
+              alert('please verify email')
+            })
         })
         .catch(error=>{
             console.error(error.message);
@@ -54,21 +74,35 @@ const Register = () => {
     <div>
     <div className="mx-auto md:w-1/2">
     <h2 className="text-3xl my-4">Please Register</h2>
+    {/* form is starting here */}
       <form onSubmit={handleSubmit}>
-        <input className="mb-4 w-3/4 py-2 px-4 bg-slate-300" type="email" name="email" id="" placeholder="Email Address" required/>
+        <input className="mb-4 w-full py-2 px-4 bg-slate-300" type="text" name="name" id="" placeholder="Your name" required/>
         <br />
-        <input
-         className="mb-4 w-3/4 py-2 px-4 bg-slate-300"
+        {/* name input field */}
+        <input className="mb-4 w-full py-2 px-4 bg-slate-300" type="email" name="email" id="" placeholder="Email Address" required/>
+        <br />
+        {/* second input field */}
+      <div className="relative">
+      <input
+         className=" w-full py-2 px-4 bg-slate-300"
           type={show?"text" : "password"} 
           name="password" 
           id="" placeholder="Password" required/>
-        <span onClick={()=>setShow(!show)}>
+        <span className="absolute mt-3 right-2" onClick={()=>setShow(!show)}>
             {
                 show?<IoEyeOutline />:<FaRegEyeSlash />
             }
         </span>
+      </div>
         <br />
-        <input className="btn btn-secondary mb-4 w-3/4" type="submit" value="Register" />
+          {/* third input field */}
+          <div className="ml-2">
+          <input type="checkbox" name="terms" id="terms" />
+          <label htmlFor="terms">Accept our <a href="">Terms and conditions</a></label>
+          </div>
+          <br />
+          {/* fourth input field */}
+        <input className="btn btn-secondary mb-4 w-full" type="submit" value="Register" />
       </form>
       {
         registerError && <p className="text-red-500">{registerError}</p>
@@ -76,6 +110,7 @@ const Register = () => {
       {
         success && <p className="text-green-600">{success}</p>
       }
+      <p>already have an account?<Link className="text-red-500" to="/login">Login</Link></p>
     </div>
 
     </div>
