6c1113994a4260cd025a45ad616caf0ae226bb39
Shahadoth-ali <md.sahadat.live@gmail.com>
2024-04-05 17:02:14 +0600

    27th commit

--- coffe-store-client/src/components/CoffeeCard.jsx
+++ coffe-store-client/src/components/CoffeeCard.jsx
@@ -1,10 +1,47 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
-const CoffeeCard = ({coffee}) => {
 
-    const {photo,name,details,quantity,supplier,taste}=coffee;
+const CoffeeCard = ({coffee,coffees,setCoffees}) => {
+
+    const {_id,photo,name,details,quantity,supplier,taste}=coffee;
+
+    const handleDelete=(id)=>{
+      console.log(id);
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+         
+          fetch(`http://localhost:5000/coffee/${id}`,{
+            method:'DELETE'
+          })
+          .then(res=>res.json())
+          .then(data=>{
+            console.log(data);
+            if(data.deletedCount>0){
+             Swal.fire({
+            title: "Deleted!",
+            text: "Your Coffee has been deleted.",
+            icon: "success"
+            
+          });
+          const remaining=coffees.filter(cof=>cof._id !==_id);
+          setCoffees(remaining);
+            }
+          })
+        }
+      });
+    }
 
   return (
     <div className="card card-side bg-base-100 shadow-xl gap-2">
@@ -23,10 +60,13 @@ const CoffeeCard = ({coffee}) => {
     
      <div className="flex flex-col mt-14 my-2 gap-2">
      <button className="btn btn-active btn-primary">View</button>
-<button className="btn btn-active btn-secondary">Edit</button>
-<button className="btn btn-active btn-accent">Delete</button>
+     <Link to={`updateCoffee/${_id}`}>
+     <button className="btn btn-active btn-secondary">Edit</button>
+     </Link>
+<button
+onClick={()=>handleDelete(_id)}
+className="btn btn-active btn-accent">Delete</button>
      </div>
-    
   </div>
 </div>
   )
--- coffe-store-client/src/components/UpdateCoffe.jsx
+++ coffe-store-client/src/components/UpdateCoffe.jsx
@@ -1,9 +1,113 @@
+/* eslint-disable no-unused-vars */
+import { useLoaderData } from "react-router-dom"
+import Swal from "sweetalert2";
 
 
 const UpdateCoffe = () => {
+
+  const coffee=useLoaderData();
+  const {_id,photo,name,details,quantity,supplier,taste,category}=coffee;
+
+
+  const handleUpdateCoffee=(e)=>{
+    e.preventDefault();
+    const form=e.target;
+    const name=form.name.value;
+    const quantity=form.quantity.value;
+    const supplier=form.supplier.value;
+    const taste=form.taste.value;
+    const category=form.category.value;
+    const details=form.details.value;
+    const photo=form.photo.value;
+
+    const updatedCoffee={name,quantity,supplier,taste,category,details,photo}
+    console.log(updatedCoffee);
+
+    //send data to the server
+    fetch(`http://localhost:5000/coffee/${_id}`,{
+        method:'PUT',
+        headers:{
+            'content-type':'application/json'
+        },
+        body:JSON.stringify(updatedCoffee)
+    })
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data);
+        if(data.modifiedCount>0){
+            Swal.fire({
+                title: 'Success',
+                text: 'Coffee Updated Successfully',
+                icon: 'success',
+                confirmButtonText: 'Cool'
+              })
+        }
+    })
+}
+
   return (
-    <div>
-      <h2>Update Coffee</h2>
+    <div className="p-24">
+      <h2 className="text-3xl font-extrabold">Update Coffee : {name}</h2>
+      <form onSubmit={handleUpdateCoffee}>
+           {/* form row--> name and quantity */}
+        <div className="md:flex gap-4 md:mb-3">
+            {/* first element */}
+        <div className="md:w-1/2">
+        <label htmlFor="">Name</label>
+        <br />
+       <input type="text" name="name" defaultValue={name} placeholder="coffeeeName" className="border w-full"/>
+        </div>
+        {/* second element */}
+        <div className="md:w-1/2">
+        <label htmlFor="">Available Quantity</label>
+        <br />
+       <input type="text" name="quantity" defaultValue={quantity} placeholder="Available Quantity" className="border w-full"/>
+        </div>
+        </div>
+
+           {/* form row---> supplier and taste */}
+        <div className="md:flex gap-4 md:mb-3">
+            {/* first element */}
+        <div className="md:w-1/2">
+        <label htmlFor="">Supplier Name</label>
+        <br />
+       <input type="text" name="supplier" defaultValue={supplier} placeholder="Supplier Name" className="border w-full"/>
+        </div>
+        {/* second element */}
+        <div className="md:w-1/2">
+        <label htmlFor="">Taste</label>
+        <br />
+       <input type="text" name="taste" defaultValue={taste} placeholder="Taste" className="border w-full"/>
+        </div>
+        </div>
+
+           {/* form row---> category and details */}
+        <div className="md:flex gap-4 md:mb-3">
+            {/* first element */}
+        <div className="md:w-1/2">
+        <label htmlFor="">Category</label>
+        <br />
+       <input type="text" name="category" defaultValue={category} placeholder="Category" className="border w-full"/>
+        </div>
+        {/* second element */}
+        <div className="md:w-1/2">
+        <label htmlFor="">Details</label>
+        <br />
+       <input type="text" name="details" defaultValue={details} placeholder="Details" className="border w-full"/>
+        </div>
+        </div>
+
+           {/* form row---> photo url */}
+        <div className="md:flex gap-4 md:mb-3">
+            {/* first element */}
+        <div className="w-full">
+        <label htmlFor="">Photo Url</label>
+        <br />
+       <input type="text" name="photo" defaultValue={photo} placeholder="Photo Url" className="border w-full"/>
+        </div>
+        </div>
+        <input type="submit" value="Update Coffee" className="btn btn-block"/>
+      </form>
     </div>
   )
 }
